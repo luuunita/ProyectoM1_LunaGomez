2d7fd7464b30454fa06b12000552254e4e95e3ad
LunaGomez24 <neontime09@gmail.com>
2026-02-25 13:18:49 -0500

    eliminacion de comentarios para entrega final

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// traemos los elementos de html usando sus id
 const botonGenerar = document.getElementById("generate");
 const selectorCantidad = document.getElementById("size");
 const contenedorColores = document.getElementById("colors-container");
@@ -18,19 +17,16 @@ function mensajeListo() {
     const cantidad= selectorCantidad.value;
     const formato = selectorFormato.value;
 
-    if (cantidad === "" || formato === "select" || formato === "") return; //si falta algo no muestra
+    if (cantidad === "" || formato === "select" || formato === "") return; 
     
     mostrarMensaje("Listo para generar: " + cantidad + " en " + formato);
 }
     selectorCantidad.addEventListener("change", mensajeListo);
     selectorFormato.addEventListener("change", mensajeListo);
 
-// el evento que va a suceder cuando el usuaio haga clic en el boton
-// cuando haya un click.....
 botonGenerar.addEventListener("click", function() { 
     console.log("clic Generar Paleta");
 
-    // leer cuantos colores quiere el usuario, ...ejecuta este bloque cuando haya clic
     let cantidad = Number(selectorCantidad.value);
     console.log("Cantidad", cantidad);
 
@@ -41,26 +37,19 @@ botonGenerar.addEventListener("click", function() {
         return;
     }
 
-    //esto es para limpiar el contenedor, los colores anteriores
     contenedorColores.innerHTML = "";
 
     function hslToRGB(h, s, l) {
-        //convertimos s y l de porcentaje a decimal
         s = s/ 100;
         l = l/ 100;
 
-        //calculamos un valor intermedio llamado c
         let c = (1 - Math.abs(2 * l - 1)) * s;
-        //calculamos otro valor intermedio llamado x
         let x = c* (1 - Math.abs((h / 60) % 2 - 1));
 
-        //calculamos un ajuste llamado m
         let m = l - c / 2;
 
-        //declaramos variables temporales
         let r1, g1, b1;
          
-        //dependiendo del rango hue asignamos valores
         if (h < 60) {
             r1 = c; g1 = x; b1 = 0
         } else if (h < 120) {
@@ -75,12 +64,10 @@ botonGenerar.addEventListener("click", function() {
             r1 = c; g1 = 0; b1 = x;
         }   
 
-        //convertimos los valores a escala 0-255
        let r = Math.round((r1 + m) * 255);
        let g = Math.round((g1 + m) * 255);
        let b = Math.round((b1 + m) * 255);
 
-        //devolvemos valores en un objeto
         return {r: r, g: g, b: b };
     }
 
@@ -88,7 +75,6 @@ botonGenerar.addEventListener("click", function() {
 
     for (let i = 0; i < cantidad; i++) {
       
-        //hsl aleatorio
         let h = Math.floor(Math.random() *360);
         let s = Math.floor(Math.random() * 101);
         let l = Math.floor(Math.random() * 101);
@@ -99,20 +85,16 @@ botonGenerar.addEventListener("click", function() {
 
         const colorRGB = "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b +")";                   
     
-        //tarjeta de color
         const tarjeta = document.createElement("div");
         tarjeta.classList.add("color-box");
         tarjeta.setAttribute("data-tooltip", "Clic para copiar");
         tarjeta.setAttribute("tabindex", "0");
 
-        //muestra de color
         const muestra = document.createElement("div");
         muestra.classList.add("color-sample");
 
-        //crear el texto del formato
         const textoFormato = document.createElement("p");
 
-        //mostrar texto segun seleccion
         if (formatoSeleccionado === "hsl") {
             muestra.style.backgroundColor = colorHSL;
             textoFormato.textContent = colorHSL;
@@ -121,7 +103,6 @@ botonGenerar.addEventListener("click", function() {
             textoFormato.textContent = colorRGB;
         }
 
-        //insertamos en el DOM
         tarjeta.appendChild(muestra);
         tarjeta.appendChild(textoFormato);
 
@@ -132,16 +113,16 @@ botonGenerar.addEventListener("click", function() {
 
      mostrarMensaje("Colores generados: " + selectorCantidad.value + " en " + selectorFormato.value);
 });
-function animarTarjetas() { //crea una funciona para animar tarjetas
-    const tarjetas = document.querySelectorAll(".color-box"); //selecciona todas las tarj
-    tarjetas.forEach((t, index) => {//recorre tarj con su posicion
-    t.style.opacity = "0";  //empieza invisible 
-    t.style.transform = "translateY(10px)"; //empieza un poco abajo
-    t.style.transition = "opacity 0.25s ease, transform 0.25s ease"; //animacion suave
-    setTimeout(()  => {  //hace una espera 
-        t.style.opacity = "1"; //aparece
-        t.style.transform = "translateY(0)"; //sube a su lugar 
-    }, 40 * index); //retraso por tarj para efecto escalonado
+function animarTarjetas() { 
+    const tarjetas = document.querySelectorAll(".color-box"); 
+    tarjetas.forEach((t, index) => {
+    t.style.opacity = "0";  
+    t.style.transform = "translateY(10px)"; 
+    t.style.transition = "opacity 0.25s ease, transform 0.25s ease"; 
+    setTimeout(()  => {  
+        t.style.opacity = "1"; 
+        t.style.transform = "translateY(0)"; 
+    }, 40 * index); 
 });
 }
 
